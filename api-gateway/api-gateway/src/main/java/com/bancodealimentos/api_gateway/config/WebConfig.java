46a8658d019e6a44a35e28340f5d04ff7a7592ee
Nathalia Aracely <nathaliaaracely.17@gmail.com>
2025-07-21 10:50:26 -0500

    Actualización completa del proyecto con nueva versión

--- api-gateway/api-gateway/src/main/java/com/bancodealimentos/api_gateway/config/WebConfig.java
+++ api-gateway/api-gateway/src/main/java/com/bancodealimentos/api_gateway/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
+
 @Configuration
 public class WebConfig implements WebFluxConfigurer {
     
