9fa317299d851b726a24ce61c17cd3aa6b03bd3f
Nathalia Aracely <nathaliaaracely.17@gmail.com>
2025-07-27 08:44:18 -0500

    eliminacion

--- api-gateway/api-gateway/src/main/java/com/bancodealimentos/api_gateway/config/WebConfig.java
+++ api-gateway/api-gateway/src/main/java/com/bancodealimentos/api_gateway/config/WebConfig.java
@@ -3,13 +3,14 @@ package com.bancodealimentos.api_gateway.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.lang.NonNull;
 
 
 @Configuration
 public class WebConfig implements WebFluxConfigurer {
     
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
